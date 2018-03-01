c053c8ee3b0286547966e2764020050833a215cc
Sinorda <605013352@qq.com>
2018-02-28 21:48:47 -0800

    modify test cases

--- test/TestMain.java
+++ test/TestMain.java
@@ -1,16 +1,16 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class TestMain {
 
 	@Test
   public void shouldFail() {
-  	assertTrue(false);
+  	assertEquals(6, 1);
   }
 	
 	@Test
   public void shouldPass() {
-  	assertTrue(true);
+  	assertEquals(6, 6);
   }
 
 }
