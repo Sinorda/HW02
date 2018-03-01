d5c5d5e3ac75afa02bd024bf1476eb32e292b103
Sinorda <605013352@qq.com>
2018-02-28 21:02:23 -0800

    add test display name

--- test/TestMain.java
+++ test/TestMain.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 public class TestMain {
 	
 	@Test
+	@DisplayName("Test true is true, should always pass.")
   public void evaluatesExpression() {
   	assertTrue(true);
   }
 
   @Test
+  @DisplayName("Test 6 == 10, should always fail.")
   public void shouldFail() {
   	assertEquals(6, 10);
   }
