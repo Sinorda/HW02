c47f3956ee2af0b7434c5b7d00ec5d2cc6e10e2c
Sinorda <605013352@qq.com>
2018-02-28 21:16:28 -0800

    asdasd

--- test/TestMain.java
+++ test/TestMain.java
@@ -2,17 +2,15 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestMain {
+
+	@Test
+  public void shouldFail() {
+  	assertTrue(false);
+  }
 	
 	@Test
-  public void evaluatesExpression() {
-  	System.out.println("Test true is true, should always pass.");
+  public void shouldPass() {
   	assertTrue(true);
-  }
-
-  @Test
-  public void shouldFail() {
-  	System.out.println("Test 6 == 10, should always fail.");
-  	assertEquals(6, 10);
   }
 
 }
