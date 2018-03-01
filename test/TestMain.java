eda72cb63f020ee2c4a46833fb3aaad9c07b82ce
Sinorda <605013352@qq.com>
2018-02-28 21:07:19 -0800

    add print statement on test cases

--- test/TestMain.java
+++ test/TestMain.java
@@ -5,11 +5,13 @@ public class TestMain {
 	
 	@Test
   public void evaluatesExpression() {
+  	System.out.println("Test true is true, should always pass.");
   	assertTrue(true);
   }
 
   @Test
   public void shouldFail() {
+  	System.out.println("Test 6 == 10, should always fail.");
   	assertEquals(6, 10);
   }
 
